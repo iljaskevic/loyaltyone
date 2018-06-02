760e075398dffa6fc5a5afb4ba1ea90b2a71bb21
Igor Ljaskevic <iljaskevic@gmail.com>
2018-06-01 23:26:07 -0400

    Initial commit

---
+++ src/main/java/com/ljaskevic/loyaltyone/LoyaltyOneAssessmentApplication.java
@@ -0,0 +1,12 @@
+package com.ljaskevic.loyaltyone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyOneAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyaltyOneAssessmentApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ljaskevic/loyaltyone/LoyaltyOneAssessmentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ljaskevic.loyaltyone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoyaltyOneAssessmentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
