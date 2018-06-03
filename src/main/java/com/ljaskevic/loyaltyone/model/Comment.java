abf95085a1a4529765b2d98e77111acbf4f806c0
Igor Ljaskevic <iljaskevic@gmail.com>
2018-06-03 13:08:41 -0400

    Added Comment model

---
+++ src/main/java/com/ljaskevic/loyaltyone/model/Comment.java
@@ -0,0 +1,20 @@
+package com.ljaskevic.loyaltyone.model;
+
+public class Comment {
+
+  private final long id;
+  private final String content;
+
+  public Comment(long id, String content) {
+      this.id = id;
+      this.content = content;
+  }
+
+  public long getId() {
+      return id;
+  }
+
+  public String getContent() {
+      return content;
+  }
+}
