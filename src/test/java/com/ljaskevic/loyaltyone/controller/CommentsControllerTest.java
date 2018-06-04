c628eb3806a6461bfe34910a3eed7fbdeac1c417
Igor Ljaskevic <iljaskevic@gmail.com>
2018-06-04 01:43:57 -0400

    Added unit test for the comments controller

---
+++ src/test/java/com/ljaskevic/loyaltyone/controller/CommentsControllerTest.java
@@ -0,0 +1,27 @@
+package com.ljaskevic.loyaltyone.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import com.ljaskevic.loyaltyone.model.Comment;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommentsControllerTest {
+
+  @Autowired
+  private CommentsController commentsController;
+
+  @Test
+  public void submitComment() throws Exception {
+    String testComment = "Test comment content";
+    Comment input = new Comment(0L, testComment);
+    Comment output = commentsController.submit(input);
+
+    assertThat(output.getId()).isEqualTo(0L);
+    assertThat(output.getContent()).isEqualTo(testComment);
+  }
+}
