1d6261f472a6b7e2d464648b8844471c9d7a8e15
Igor Ljaskevic <iljaskevic@gmail.com>
2018-06-06 18:55:43 -0400

    Adjusted JS cript for pulling content from back end

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,38 +1,68 @@
 $(function() {
 
-  function getCommentTemplate(text) {
-    var comTemplate = `<div class="comment">
-    <div class="content">${text}</div>
-    <div class="replies"></div>
-    </div>`;
-    return comTemplate;
-  }
+  var loyaltyone = {
+    getCommentTemplate: function(text) {
+      var comTemplate = `<div class="comment">
+      <div class="content">${text}</div>
+      <div class="replies"></div>
+      </div>`;
+      return comTemplate;
+    },
 
-  $('#add-comment-btn-post').click(function() {
-    var comment = {
-      content: $('textarea#add-comment-text').val()
-    };
+    loadRootComments: function() {
+      $.ajax({
+        type: 'GET',
+        url: '/api/comments',
+        contentType: 'application/json',
+        dataType: 'json',
+        success: function(data){
+          data.forEach(function(item) {
+            let newComment = loyaltyone.getCommentTemplate(data.content);
+            $('.comments').append(newComment);
+            $('textarea#add-comment-text').val('');
+          });
+        },
+        failure: function(errMsg) {
+          console.log( errMsg );
+        }
+      });
+    },
 
-    if (comment.content.trim() === "") return;
+    addHandlers: function() {
+      $('#add-comment-btn-post').click(function() {
+        var comment = {
+          content: $('textarea#add-comment-text').val()
+        };
 
-    $.ajax({
-      type: 'POST',
-      url: '/api/comments',
-      data: JSON.stringify(comment),
-      contentType: 'application/json',
-      dataType: 'json',
-      success: function(data){
-        var newComment = getCommentTemplate(data.content);
-        $('.comments').append(newComment);
+        if (comment.content.trim() === "") return;
+
+        $.ajax({
+          type: 'POST',
+          url: '/api/comments',
+          data: JSON.stringify(comment),
+          contentType: 'application/json',
+          dataType: 'json',
+          success: function(data){
+            var newComment = loyaltyone.getCommentTemplate(data.content);
+            $('.comments').prepend(newComment);
+            $('textarea#add-comment-text').val('');
+          },
+          failure: function(errMsg) {
+            console.log( errMsg );
+          }
+        });
+      });
+
+      $('#add-comment-btn-cancel').click(function() {
         $('textarea#add-comment-text').val('');
-      },
-      failure: function(errMsg) {
-        console.log( errMsg );
-      }
-    });
-  });
-  $('#add-comment-btn-cancel').click(function() {
-    $('textarea#add-comment-text').val('');
-  });
+      });
+    },
 
+    init: function() {
+      loyaltyone.addHandlers();
+      loyaltyone.loadRootComments();
+    }
+  };
+
+  loyaltyone.init();
 });
