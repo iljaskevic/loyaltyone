1e06553556dc01583fef344714c61e4ceeea5d62
Igor Ljaskevic <iljaskevic@gmail.com>
2018-06-06 18:05:38 -0400

    Added date to the stored comment

--- src/main/java/com/ljaskevic/loyaltyone/models/Comment.java
+++ src/main/java/com/ljaskevic/loyaltyone/models/Comment.java
@@ -1,24 +1,28 @@
 package com.ljaskevic.loyaltyone.models;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import java.util.Date;
 
 public class Comment {
 
-    private final String id;
+    public final String id;
 
-    private final String parentId;
-    private final String content;
+    public String parentId;
+    public String content;
+    public Date dateCreated;
 
     public Comment() {
         this.id = RandomStringUtils.randomAlphanumeric(8);
         this.parentId = "0";
         this.content = "";
+        this.dateCreated = new Date();
     }
 
     public Comment(String parentId, String content) {
         this.id = RandomStringUtils.randomAlphanumeric(8);
         this.parentId = parentId;
         this.content = content;
+        this.dateCreated = new Date();
     }
 
     public String getId() {
@@ -31,5 +35,9 @@ public class Comment {
 
     public String getContent() {
         return content;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
     }
 }
